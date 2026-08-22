0ffc48d09d15518ce941f8c2b909ebb39014e82d
Brian Hickey <90266950+bhickey777@users.noreply.github.com>
2026-08-21 22:13:13 -0500

    Comment out Thread.sleep in Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
         System.out.println("Container is up. Sleeping so you can docker ps / docker logs / docker exec into it.");
-        Thread.sleep(600_000);
+        //Thread.sleep(600_000);
     }
 }
