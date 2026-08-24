a15783dc3fe7ba5a24214e2e0c57f879ac527d46
ilhan2316 <ilhgelle@gmail.com>
2026-08-24 19:42:27 +0000

    feat: added springboot files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(Main.class, args);
 
         System.out.println("Container is up. Sleeping so you can docker ps / docker logs / docker exec into it.");
         //Thread.sleep(600_000);
